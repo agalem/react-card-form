89ca348bf5c669fa3322c18c6b6be1415cef6aaf
Agnieszka Lempaszek <agnieszka.lempaszek@student.uj.edu.pl>
2019-12-01 15:56:21 +0100

    Updates

---
+++ precache-manifest.4e619f2abd3a4351f85de4b23d6230a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e080e1888cea686ce9e8822b91b67ef2",
+    "url": "/react-card-form/index.html"
+  },
+  {
+    "revision": "39af72a3fbf48f887531",
+    "url": "/react-card-form/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "9ef59f596d05a56acd83",
+    "url": "/react-card-form/static/css/main.0d968baa.chunk.css"
+  },
+  {
+    "revision": "39af72a3fbf48f887531",
+    "url": "/react-card-form/static/js/2.56b23630.chunk.js"
+  },
+  {
+    "revision": "9ef59f596d05a56acd83",
+    "url": "/react-card-form/static/js/main.0a99991f.chunk.js"
+  },
+  {
+    "revision": "276e1238707b01bba275",
+    "url": "/react-card-form/static/js/runtime-main.bcb6efd6.js"
+  },
+  {
+    "revision": "5b2a3e2910e186151a6557042383fbe5",
+    "url": "/react-card-form/static/media/background.5b2a3e29.jpeg"
+  },
+  {
+    "revision": "2c37b1d33b45b2158112bb8172af579b",
+    "url": "/react-card-form/static/media/chip.2c37b1d3.png"
+  },
+  {
+    "revision": "62706277b8600fc43afcdf69be858c07",
+    "url": "/react-card-form/static/media/discover.62706277.png"
+  },
+  {
+    "revision": "cdaba15c87239cfe3126dda858c2078f",
+    "url": "/react-card-form/static/media/mastercard.cdaba15c.png"
+  }
+]);
